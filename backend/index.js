2328bcaaca79b135725c335b2c51fd0c8d8d88c3
Quincygithub6 <129473729+Quincygithub6@users.noreply.github.com>
2025-11-30 15:59:29 -0500

    Fix: use process.env.PORT.

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,23 @@
 
 require('dotenv').config();
 
-const port = (() => {
-    const args = process.argv;
+// const port = (() => {
+//     const args = process.argv;
 
-    if (args.length !== 3) {
-        console.error("usage: node index.js port");
-        process.exit(1);
-    }
+//     if (args.length !== 3) {
+//         console.error("usage: node index.js port");
+//         process.exit(1);
+//     }
 
-    const num = parseInt(args[2], 10);
-    if (isNaN(num)) {
-        console.error("error: argument must be an integer.");
-        process.exit(1);
-    }
+//     const num = parseInt(args[2], 10);
+//     if (isNaN(num)) {
+//         console.error("error: argument must be an integer.");
+//         process.exit(1);
+//     }
 
-    return num;
-})();
+//     return num;
+// })();
+const port = process.env.PORT || 3000;
 
 const express = require("express");
 const cors = require('cors'); //
